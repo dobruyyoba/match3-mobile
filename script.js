ddb51c0c60c97cb744b562aa50ea388613325003
Zaytsev Stepan <zaytsevstepan17@gmail.com>
2025-03-18 19:48:27 +0300

    TRY to fix 3

--- script.js
+++ script.js
@@ -3,43 +3,12 @@ tg.expand();
 
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
-let startX = 0;
-let startY = 0;
-let endX = 0;
-let endY = 0;
-let draggedButton = null;
 
-button1.addEventListener('touchstart', handleTouchStart);
-button2.addEventListener('touchstart', handleTouchStart);
+const hammer1 = new Hammer(button1);
+const hammer2 = new Hammer(button2);
 
-function handleTouchStart(event) {
-    startX = event.touches[0].clientX;
-    startY = event.touches[0].clientY;
-    draggedButton = event.target;
-    document.addEventListener('touchend', handleTouchEnd); // Добавляем обработчик touchend на документ
-}
-
-document.addEventListener('touchmove', handleTouchMove);
-
-function handleTouchMove(event) {
-    endX = event.touches[0].clientX;
-    endY = event.touches[0].clientY;
-}
-
-function handleTouchEnd() {
-    const deltaX = endX - startX;
-    const deltaY = endY - startY;
-
-    if (Math.abs(deltaX) > 50 || Math.abs(deltaY) > 50) {
-        if (draggedButton === button1 && deltaX > 50) {
-            swapButtons();
-        } else if (draggedButton === button2 && deltaX < -50) {
-            swapButtons();
-        }
-    }
-
-    document.removeEventListener('touchend', handleTouchEnd); // Удаляем обработчик touchend
-}
+hammer1.on('swipeleft', swapButtons);
+hammer2.on('swiperight', swapButtons);
 
 function swapButtons() {
     const tempText = button1.textContent;
