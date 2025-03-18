2001c6be335724268d5d1f4fe93c3e654f620bf6
Zaytsev Stepan <zaytsevstepan17@gmail.com>
2025-03-18 20:15:54 +0300

    TRY to fix 5

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const tg = window.Telegram.WebApp;
-tg.expand();
-
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 
 const hammer1 = new Hammer(button1);
 const hammer2 = new Hammer(button2);
 
-hammer1.on('swipeleft', swapButtons);
-hammer2.on('swiperight', swapButtons);
+hammer1.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
+hammer2.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
 
-function swapButtons() {
-    const tempText = button1.textContent;
-    button1.textContent = button2.textContent;
-    button2.textContent = tempText;
+hammer1.on('swipeleft', () => swapButtons(button1, button2));
+hammer2.on('swiperight', () => swapButtons(button2, button1));
+
+function swapButtons(btn1, btn2) {
+    const tempText = btn1.textContent;
+    btn1.textContent = btn2.textContent;
+    btn2.textContent = tempText;
 }
